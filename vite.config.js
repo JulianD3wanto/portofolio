b853a135c7ed216c4185daa8191f2ac34400c5bd
JulianD3wanto <juliandewanto@student.uns.ac.id>
2026-03-15 07:16:22 +0700

    deploy gh io2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/julian-portofolio",
   plugins: [react(), tailwindcss()],
+  base: '/julian-portofolio/'
 })
